4bc8f77651c7862928536610711dad42ce26239a
lobatojorge <jorgelobatom@gmail.com>
2026-01-11 17:57:05 +0000

    Añadidos archivos de datos de cancer de mama

--- PEC4.R
+++ PEC4.R
@@ -1,3 +1,9 @@
+*Sección 1. Contexto y pautas de estudio*
+*Especificad la procedencia o el origen de los datos, incluid las referencias
+correspondientes y justificad por qué habéis elegido estos datos, indicando qué objetivos
+o preguntas queréis responder*
 
-plot(1)
-plot(2)
+
+
+*Sección 2. Prospección y preparación de los datos
+
